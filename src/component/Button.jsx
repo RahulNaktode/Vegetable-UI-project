839307f69bb161de12e312855c646b849b773fc3
RahulNaktode <rahulnaktode04@gmail.com>
2025-11-27 15:30:44 +0530

    add button

---
+++ src/component/Button.jsx
@@ -0,0 +1,22 @@
+const BUTTON_VARIETY = {
+    primary: "bg-orange-600 text-white hover:bg-orange-700 transition-duration-700",
+    secondary: "bg-blue-500 text-white hover:bg-blue-600 transition-duration-700",
+    thrid: "bg-green-700 text-white hover:bg-green-800 transition-duration-700"
+}
+
+const BUTTON_SIZE = {
+    larger: "text-3xl px-7 py-1.5",
+    medium: "text-2xl px-5 py-1",
+    small: "text-xl px-3 py-0.5"
+}
+
+function Button({title, onClick = () => {}, varient='primary', size='larger'}) {
+  return (
+    <button 
+    onClick={onClick}
+    className={`rounded-full ${BUTTON_VARIETY[varient]} ${BUTTON_SIZE[size]} mx-5 mt-10 cursor-pointer`}
+    >{title}</button>
+  )
+}
+
+export default Button
--- src/view/Home.jsx
+++ src/view/Home.jsx
@@ -1,7 +1,7 @@
 import Navbar from '../component/Navbar.jsx'
 import Footer from '../component/Footer.jsx'
 import VegetableImg from './../assets/vegetables.png'
-
+import Button from './../component/Button.jsx'
 import FeatureSession from '../component/FeatureSession.jsx'
 
 
@@ -11,9 +11,15 @@ function Home() {
     <div>
       <Navbar />
       <div className='min-h-screen'>
-        <div className='items-center md:flex px-10 md:px-25'>
+        <div className='items-center md:flex px-10 md:px-25 md:my-15'>
           <img src={VegetableImg} alt="Vegetable-image" className='pl-20 h-50 md:h-90'/>
-          <h1 className='text-center text-4xl! md:text-7xl!  text-orange-700'>One Platfrom that all the Vegetables you Needs</h1>
+          <div>
+          <h1 className='text-center text-4xl! md:text-6xl! md:ml-20  text-orange-700'>One Platfrom that all the Vegetables you Needs</h1>
+           <div className='flex flex-col justify-center md:ml-10 md:flex md:flex-row'>
+           <Button title='Explore Now' varient='primary' size='medium'/>
+           <Button title='Call Us' varient='thrid' size='medium'/>
+           </div>
+           </div>
         </div>
         
         <FeatureSession />
